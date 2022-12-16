c154e59a4aea998371d7379836f0f68bce1904a1
Van Anh <1910115@dlu.edu.vn>
2022-12-16 08:27:34 +0700

    vanh da o day

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -1,5 +1,5 @@
 import authorizedAxiosInstance from '../../utils/customAxios'
-import { API_ROOT } from '../../utils/constants'
+import { API_ROOT_GOLANG } from '../../utils/constants'
 
 // export const updateBoardAPI = async (id, data) => {
 //   const request = await authorizedAxiosInstance.put(`${API_ROOT}/v1/boards/${id}`, data)
@@ -7,6 +7,6 @@ import { API_ROOT } from '../../utils/constants'
 // }
 
 export const checkHealth = async () => {
-  const request = await authorizedAxiosInstance.put(`${API_ROOT}/status`)
+  const request = await authorizedAxiosInstance.put(`${API_ROOT_GOLANG}/status`)
   return request.data
 }
